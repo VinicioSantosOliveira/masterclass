897424f8e5426fa829700e91280d30e9b3e7cdfc
Vinicio Oliveira <viniciodossantosoliveira@hotmail.com>
2024-01-10 20:26:49 -0300

    code

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: HomePage(),
+      home: CubePage(),
     );
   }
 }
@@ -23,10 +23,81 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: const Color.fromARGB(255, 251, 0, 0),
+        title: const Text('Home Page'),
       ),
       body: const Center(
         child: Text(
           'Masterclass',
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+class UserPage extends StatelessWidget {
+  const UserPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: const Color.fromARGB(255, 251, 0, 0),
+        title: const Text('User Page'),
+      ),
+      body: const Center(
+        child: Text(
+          'User Page',
+        ),
+      ),
+    );
+  }
+}
+
+class CubePage extends StatelessWidget {
+  const CubePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: const Color.fromARGB(255, 251, 0, 0),
+        title: const Text('Cube Page'),
+      ),
+      body: SizedBox(
+        width: double.infinity,
+        height: double.infinity,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              width: 100,
+              height: 100,
+              color: Colors.yellow,
+            ),
+            const SizedBox(
+              height: 10,
+              width: 10,
+            ),
+            Container(
+              width: 100,
+              height: 100,
+              color: Colors.green,
+            ),
+            const SizedBox(
+              height: 0,
+              width: 10,
+            ),
+            Container(
+              width: 100,
+              height: 100,
+              color: Colors.blue,
+            ),
+          ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
