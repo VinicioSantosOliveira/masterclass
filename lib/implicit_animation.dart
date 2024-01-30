de4d334ce71db6b6f7a8b50c527ff7cbb33a3d7a
Vinicio Oliveira <viniciodossantosoliveira@hotmail.com>
2024-01-29 21:02:14 -0300

    desafio concluido

--- lib/implicit_animation.dart
+++ lib/implicit_animation.dart
@@ -8,12 +8,45 @@ class ImplicitAnimation extends StatefulWidget {
 }
 
 class _ImplicitAnimationState extends State<ImplicitAnimation> {
+  var isSelect = true;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(backgroundColor: Colors.lightBlue),
-      body: AnimatedContainer(
-        duration: const Duration(seconds: 2),
+      appBar: AppBar(
+        title: const Text(
+          'Desafio do botao flutuante',
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        backgroundColor: Colors.lightBlue,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(30.0),
+        child: GestureDetector(
+          onTap: () {
+            setState(() {
+              isSelect = !isSelect;
+            });
+          },
+          child: AnimatedAlign(
+            duration: const Duration(seconds: 2),
+            alignment: isSelect ? Alignment.bottomRight : Alignment.topCenter,
+            child: AnimatedContainer(
+              duration: const Duration(seconds: 2),
+              height: 50,
+              width: isSelect ? 50 : 100,
+              decoration: BoxDecoration(
+                color: Colors.lightBlue,
+                borderRadius: BorderRadius.all(
+                  isSelect ? const Radius.circular(40) : Radius.zero,
+                ),
+              ),
+            ),
+          ),
+        ),
       ),
     );
   }
