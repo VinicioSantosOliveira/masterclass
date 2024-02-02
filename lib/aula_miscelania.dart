4f8dbef8e415f2331441f65185ca496e714465fb
Vinicio Oliveira <viniciodossantosoliveira@hotmail.com>
2024-02-02 04:57:47 -0300

    fix: arrumei o myApp

--- lib/aula_miscelania.dart
+++ lib/aula_miscelania.dart
@@ -8,23 +8,122 @@ class Miscelania extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(),
       body: ListView(
+        scrollDirection: Axis.vertical,
         children: [
           Align(
-            child: Container(
-              height: 300,
-              width: 300,
-              color: Colors.red,
+            child: Padding(
+              padding: const EdgeInsets.only(bottom: 10),
+              child: Container(
+                height: 80,
+                width: 300,
+                color: Colors.red,
+              ),
             ),
           ),
-          Container(
-            height: 300,
-            width: 300,
-            color: Colors.yellow,
+          Padding(
+            padding: const EdgeInsets.only(bottom: 10),
+            child: SizedBox(
+              height: 100,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children: [
+                  Align(
+                    child: Padding(
+                      padding: const EdgeInsets.only(right: 10),
+                      child: Container(
+                        height: 50,
+                        width: 100,
+                        color: Colors.blue,
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(right: 10),
+                    child: Container(
+                      width: 100,
+                      color: Colors.blue,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(right: 10),
+                    child: Container(
+                      width: 100,
+                      color: Colors.blue,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(right: 10),
+                    child: Container(
+                      width: 100,
+                      color: Colors.blue,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(right: 10),
+                    child: Container(
+                      width: 100,
+                      color: Colors.blue,
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ),
-          Container(
-            height: 300,
-            width: 300,
-            color: Colors.green,
+          Padding(
+            padding: const EdgeInsets.only(bottom: 10),
+            child: Container(
+              height: 80,
+              width: 300,
+              color: Colors.green,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(bottom: 10),
+            child: Container(
+              height: 80,
+              width: 300,
+              color: Colors.green,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(bottom: 10),
+            child: Container(
+              height: 80,
+              width: 300,
+              color: Colors.green,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(bottom: 10),
+            child: Container(
+              height: 80,
+              width: 300,
+              color: Colors.green,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(bottom: 10),
+            child: Container(
+              height: 80,
+              width: 300,
+              color: Colors.green,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(bottom: 10),
+            child: Container(
+              height: 80,
+              width: 300,
+              color: Colors.green,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(bottom: 10),
+            child: Container(
+              height: 80,
+              width: 300,
+              color: Colors.green,
+            ),
           ),
         ],
       ),
