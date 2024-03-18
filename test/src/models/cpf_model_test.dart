908b611e8502bb3d154bed2e2d5809b6668f536a
Vinicio Oliveira <viniciodossantosoliveira@hotmail.com>
2024-03-18 12:58:05 -0300

    add: tests function isValid and errors cpfModel

--- test/src/models/cpf_model_test.dart
+++ test/src/models/cpf_model_test.dart
@@ -1,11 +1,28 @@
+import 'package:flutter_test/flutter_test.dart';
 import 'package:masterclass/src/models/cpf_model.dart';
 
 void main() {
-  CpfModel cpf = CpfModel('74712383054');
-  CpfModel cpf2 = CpfModel('74712383055');
-  CpfModel cpf3 = CpfModel('11111111111');
+  group('isValid', () {
+    test('o cpf deve ser valido', () {
+      CpfModel cpf = CpfModel('74712383054');
+      expect(cpf.isValid(), true);
+    });
+    test('o cpf deve ser invalido', () {
+      CpfModel cpf2 = CpfModel('74712383055');
+      expect(cpf2.isValid(), false);
+    });
+    test('o cpf deve ser invalido quando todos os digitos sao ==', () {
+      CpfModel cpf3 = CpfModel('11111111111');
+      expect(cpf3.isValid(), false);
+    });
+  });
 
-  print(cpf.isValid());
-  print(cpf2.isValid());
-  print(cpf3.isValid());
+  group('Erros de instancias', () {
+    test('deve ser invalido quando a quantia de numeros digitados for menor que 11 digitos', () {
+      expect(() => CpfModel('2222222222'), throwsAssertionError);
+    });
+    test('deve ser invalido quando a quantia de numeros digitados for maior que 11 digitos', () {
+      expect(() => CpfModel('222222222222'), throwsAssertionError);
+    });
+  });
 }
